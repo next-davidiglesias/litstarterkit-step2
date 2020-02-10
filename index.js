1337dde76e858cde58f4e425380eebe50b4f5067
next-davidiglesias <david.iglesias.zavala.next@bbva.com>
2020-02-10 11:20:03 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 // Import lit-html functions
 import {html, render} from 'lit-html';
 
-const items = [1,2,3,4,5]
+const items = [{srcset: '', src:''},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Defenders-of-the-moon-tee.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Defenders-of-the-moon-tee.jpg'},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Statix-Boy-Black-Tee.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Statix-Boy-Black-Tee.jpg'},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Discover-Paradise-Vintage-Black-Scoop.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Discover-Paradise-Vintage-Black-Scoop.jpg'},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/The-Archer-Tee-BLACK.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/The-Archer-Tee-BLACK.jpg'},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Strange-World-Black-Tee.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Strange-World-Black-Tee.jpg'},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Alchemist-Returns-Tee-BLACK.webp 1x', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Alchemist-Returns-Tee-BLACK.jpg'},
+{srcset: 'https://intrinsic-layout-guide.glitch.me/img/tees/Shred-Seattle-Tee.webp 1x"', src:'https://intrinsic-layout-guide.glitch.me/img/tees/Shred-Seattle-Tee.jpg'},]
+
+
 const myTemplate = ()=> html`
 <section class="horizontal-snap">
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/Defenders-of-the-moon-tee.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/Defenders-of-the-moon-tee.jpg" loading="lazy">
-  </picture></a>
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/Statix-Boy-Black-Tee.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/Statix-Boy-Black-Tee.jpg" loading="lazy">
-  </picture></a>
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/Discover-Paradise-Vintage-Black-Scoop.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/Discover-Paradise-Vintage-Black-Scoop.jpg" loading="lazy">
-  </picture></a>
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/The-Archer-Tee-BLACK.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/The-Archer-Tee-BLACK.jpg" loading="lazy">
-  </picture></a>
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/Strange-World-Black-Tee.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/Strange-World-Black-Tee.jpg" loading="lazy">
-  </picture></a>
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/Alchemist-Returns-Tee-BLACK.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/Alchemist-Returns-Tee-BLACK.jpg" loading="lazy">
-  </picture></a>
-  <a href="https://www.tenhundredart.com/"><picture>
-    <source type="image/webp" srcset="https://intrinsic-layout-guide.glitch.me/img/tees/Shred-Seattle-Tee.webp 1x">
-    <img src="https://intrinsic-layout-guide.glitch.me/img/tees/Shred-Seattle-Tee.jpg" loading="lazy">
-  </picture></a>
+  ${items.map((item)=> html`<a><picture>
+    <source type="image/webp" srcset="${item.srcset}">
+    <img src="${item.src}" loading="lazy">
+  </picture></a>`)}
 </section>
 
 
